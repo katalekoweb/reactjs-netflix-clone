21e9a05c30e27371c6102235da55bc6d6c7361da
Kataleko <katalekoweb@gmail.com>
2025-08-14 10:58:56 +0100

    feat: header and hero section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
+import serachIcon from '../../assets/search_icon.svg'
+import bellIcon from '../../assets/bell_icon.svg'
+import profileIcon from '../../assets/profile_img.png'
+import caretIcon from '../../assets/caret_icon.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <div className="navbar">
+        <div className="navbar-left">
+          <img src={logo} alt="" />
+
+          <ul>
+            <li>Home</li>
+            <li>Tv Shows</li>
+            <li>Movies</li>
+            <li>New & Popular</li>
+            <li>My List</li>
+            <li>Browser by languages</li>
+          </ul>
+        </div>
+        <div className="navbar-right">
+          <img src={serachIcon} className='icons' alt="" />
+          <p>Children</p>
+          <img src={bellIcon} className='icons' alt="" />
+
+          <div className="navbar-profile">
+            <img src={profileIcon} className='profile' alt="" />
+            <img src={caretIcon} className='' alt="" />
+            
+            <div className="dropdown">
+              <p>Sign Out of Netflix</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+import HeroBanner from '../../assets/hero_banner.jpg'
+import HeroTitle from '../../assets/hero_title.png'
+import PlayIcon from '../../assets/play_icon.png'
+import InfoIcon from '../../assets/info_icon.png'
 
 const Home = () => {
   return (
     <div className='home'>
-      
+      <Navbar />
+      <div className="hero">
+        <img src={HeroBanner} alt="" className='banner-img' />
+        <div className="hero-caption">
+          <img src={HeroTitle} alt="" className='caption-img' />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis optio saepe, dolorem quod doloribus, mollitia accusantium corporis blanditiis eum, qui repellat! Possimus iusto sunt dolore fugit animi, quasi voluptas quidem!
+          </p>
+
+          <div className="hero-btns">
+            <button className='btn'>
+              <img src={PlayIcon} alt="" /> Play
+            </button>
+            <button className='btn dark-btn'>
+              <img src={InfoIcon} alt="" /> Info
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
